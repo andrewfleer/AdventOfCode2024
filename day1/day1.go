ff68acfb951e5212bc22b6133f047e0122109a9b
Fleer, Andrew John <andrew.fleer@gmail.com>
2024-12-01 18:05:14 -0600

    Day 1 - Part 2

--- day1/day1.go
+++ day1/day1.go
@@ -4,52 +4,53 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"math"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	file, err := os.Open("../files/day1.txt")
+	file, err := os.Open("../files/day1/day1.txt")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	totalDistance := 0
+	totalScore := 0
 	var leftArray []int
-	var rightArray []int
+	var rightMap = make(map[int]int)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		leftArray, rightArray = parseLine(scanner.Text(), leftArray, rightArray)
+		leftArray, rightMap = parseLine(scanner.Text(), leftArray, rightMap)
 
 	}
 
-	for i, leftVal := range leftArray {
-		rightVal := rightArray[i]
-
-		distance := math.Abs(float64(leftVal) - float64(rightVal))
-		totalDistance += int(distance)
+	for _, leftVal := range leftArray {
+		score := calculateSimilarityScore(leftVal, rightMap)
+		totalScore += score
 	}
 
-	fmt.Println(totalDistance)
+	fmt.Println(totalScore)
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func parseLine(text string, leftArray, rightArray []int) ([]int, []int) {
+func calculateSimilarityScore(leftVal int, rightMap map[int]int) int {
+	frequency := rightMap[leftVal]
+
+	return leftVal * frequency
+}
+
+func parseLine(text string, leftArray []int, rightMap map[int]int) ([]int, map[int]int) {
 	numbers := strings.Fields(text)
 
 	leftInt, _ := strconv.Atoi(numbers[0])
 	rightInt, _ := strconv.Atoi(numbers[1])
 
 	leftArray = append(leftArray, leftInt)
-	rightArray = append(rightArray, rightInt)
 
-	sort.Ints(leftArray)
-	sort.Ints(rightArray)
-	return leftArray, rightArray
+	rightMap[rightInt] = rightMap[rightInt] + 1
+
+	return leftArray, rightMap
 }
