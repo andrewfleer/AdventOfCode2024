644e6603b29e98d22304a77dd595acdb0647f76e
Fleer, Andrew John <andrew.fleer@gmail.com>
2024-12-21 00:39:02 -0600

    Day 14 - Part 2

--- day14/day14.go
+++ day14/day14.go
@@ -3,11 +3,18 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"image/color"
 	"log"
 	"math"
 	"os"
 	"regexp"
 	"strconv"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+	"gonum.org/v1/plot/vg/vgimg"
 )
 
 type Robot struct {
@@ -55,14 +62,115 @@ func main() {
 
 	mapWidth := float64(101)
 	mapHeight := float64(103)
-	seconds := 100
+	seconds := 101 * 103
+
+	const rows, cols = 10, 10
+	rowNum := 0
+	colNum := 0
+	plots := make([][]*plot.Plot, rows)
+
 	for i := 1; i <= seconds; i++ {
+		if colNum == 0 {
+			plots[rowNum] = make([]*plot.Plot, cols)
+		}
+
 		moveRobots(robots, mapWidth, mapHeight)
+
+		p, err := createScatterPlot(robots, i, mapWidth, mapHeight)
+		if err != nil {
+			panic(err)
+		}
+
+		plots[rowNum][colNum] = p
+
+		colNum++
+		if colNum == cols {
+			rowNum++
+			colNum = 0
+		}
+
+		if rowNum == rows {
+			// save the image
+			img := vgimg.New(vg.Points(2000), vg.Points(2000))
+			dc := draw.New(img)
+
+			t := draw.Tiles{
+				Rows: rows,
+				Cols: cols,
+				PadX: vg.Millimeter,
+				PadY: vg.Millimeter,
+			}
+			canvases := plot.Align(plots, t, dc)
+
+			for y := 0; y < rows; y++ {
+				for x := 0; x < cols; x++ {
+					fmt.Printf("Printing %d row %d column\n", y, x)
+					if plots[y][x] != nil {
+						plots[y][x].Draw(canvases[y][x])
+						w, err := os.Create("robits.png")
+						if err != nil {
+							panic(err)
+						}
+
+						png := vgimg.PngCanvas{Canvas: img}
+						if _, err := png.WriteTo(w); err != nil {
+							panic(err)
+						}
+					}
+				}
+			}
+			colNum = 0
+			rowNum = 0
+		}
+
 	}
 
 	safetyScore := calculateSafetyScore(robots, mapWidth, mapHeight)
 
 	fmt.Println(safetyScore)
+}
+
+func createScatterPlot(robots []*Robot, seconds int, mapWidth, mapHeight float64) (*plot.Plot, error) {
+	p := plot.New()
+
+	// Disable all axis annotations
+	p.X.Label.Text = ""
+	p.Y.Label.Text = ""
+	p.X.Padding = 0
+	p.Y.Padding = 0
+
+	// Set ranges before disabling ticks
+	p.X.Min, p.X.Max = 0, 10
+	p.Y.Min, p.Y.Max = 0, 10
+
+	// Disable ticks and grid
+	p.X.Tick.Width = 0
+	p.Y.Tick.Width = 0
+	p.X.Width = 0
+	p.Y.Width = 0
+
+	p.Title.Text = strconv.Itoa(seconds) + " seconds"
+
+	points := make(plotter.XYs, len(robots))
+	for r, robot := range robots {
+		points[r].X = robot.x
+		points[r].Y = robot.y
+	}
+
+	scatter, err := plotter.NewScatter(points)
+	if err != nil {
+		return nil, err
+	}
+
+	scatter.GlyphStyle.Color = color.RGBA{R: 255, A: 255}
+	scatter.GlyphStyle.Radius = vg.Points(2)
+
+	p.Add(scatter)
+	p.X.Min, p.X.Max = 0, mapWidth
+	p.Y.Min, p.Y.Max = 0, mapHeight
+
+	return p, nil
+
 }
 
 func calculateSafetyScore(robots []*Robot, mapWidth, mapHeight float64) int {
